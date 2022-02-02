07d3eeb40a81b7f18563bf47111d4dbd3c4e4e48
FlGoldschmitt <79806246+FlGoldschmitt@users.noreply.github.com>
2022-02-02 16:46:26 +0100

    Creating the exceptions returned by the API

---
+++ FG.FFTT.Api.Client/Exceptions/ApiInvalidParametersException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FG.FFTT.Api.Client.Properties;
+
+namespace FG.FFTT.Api.Client.Exceptions
+{
+    /// <summary>
+    /// Type of exception thrown when a required parameter is empty or incorrect.
+    /// </summary>
+    public class ApiInvalidParametersException : ApiException
+    {
+        public ApiInvalidParametersException(string details, Exception innerException = null)
+            : base($"{Resources.InvalidParametersMessage} {details}", innerException)
+        { }
+    }
+}
---
+++ FG.FFTT.Api.Client/Exceptions/ApiUnauthorizedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FG.FFTT.Api.Client.Properties;
+
+namespace FG.FFTT.Api.Client.Exceptions
+{
+    /// <summary>
+    /// Type of exception thrown when the identification parameters (serie, tm, tmc and id) are missing or incorrect.
+    /// </summary>
+    public class ApiUnauthorizedException : ApiException
+    {
+        public ApiUnauthorizedException(string details, Exception innerException = null)
+            : base($"{Resources.UnauthorizedMessage} {details}", innerException)
+        { }
+    }
+}
